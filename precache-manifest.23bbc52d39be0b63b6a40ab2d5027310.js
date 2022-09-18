13029d7215e3b54f3abfd57c473d11baee5bebe7
Lukasz Zywluk <luke9966@gmail.com>
2022-09-18 10:46:48 -0400

    Updates

---
+++ precache-manifest.23bbc52d39be0b63b6a40ab2d5027310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1ff5c1d9f9708e29e53ce63d48abf1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "c186cbbaebbc51b5b0c5",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "c186cbbaebbc51b5b0c5",
+    "url": "/calculator/static/js/main.742c825c.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
